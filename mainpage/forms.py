b19c6e52b8b9a48c642ab3b83280d9d7b07aba4d
Danil <danilbogomolov.1710@gmail.com>
2021-05-08 03:34:33 +0300

    change forms required

--- mainpage/forms.py
+++ mainpage/forms.py
@@ -2,90 +2,90 @@ from django import forms
 from .models import *
 
 class AddConceptForm(forms.Form):
-	new_concept_name = forms.CharField(max_length = 30, label='Concept', required=False)
+	new_concept_name = forms.CharField(max_length = 30, label='Concept')
 
 class SaveForm(forms.Form):
-	file_name = forms.CharField(max_length = 30, label='File name', required=False)
+	file_name = forms.CharField(max_length = 30, label='File name')
 
 class OpenForm(forms.Form):
-	file = forms.FileField( label='File name', required=False)
+	file = forms.FileField( label='File name')
 
 class CommentForm(forms.Form):
-	text = forms.CharField(max_length = 1200, label='', required=False, widget=forms.Textarea())
+	text = forms.CharField(max_length = 1200, label='', widget=forms.Textarea())
 
 class addConceptIndividualAttrForm(forms.Form):
-	new_attribute_name = forms.CharField(max_length = 30, label='Attribute name', required=False)
-	new_attribute_value = forms.CharField(max_length = 30, label='Attribute value', required=False)
-	concept_for_attribute_name = forms.CharField(max_length = 30, label='класс атрибута', required=False, widget = forms.HiddenInput())
-	individual_for_attribute_name = forms.CharField(max_length = 30, label='экземпляр атрибута', required=False, widget = forms.HiddenInput())
+	new_attribute_name = forms.CharField(max_length = 30, label='Attribute name')
+	new_attribute_value = forms.CharField(max_length = 30, label='Attribute value')
+	concept_for_attribute_name = forms.CharField(max_length = 30, label='класс атрибута', widget = forms.HiddenInput())
+	individual_for_attribute_name = forms.CharField(max_length = 30, label='экземпляр атрибута', widget = forms.HiddenInput())
 
 class addSubConceptIndividualAttrForm(forms.Form):
-	new_attr_name = forms.CharField(max_length = 30, label='Attribute name', required=False)
-	new_attr_value = forms.CharField(max_length = 30, label='Attribute value', required=False)
-	subconc_for_attribute_name = forms.CharField(max_length = 30, label='подкласс атрибута', required=False, widget = forms.HiddenInput())
-	conc_for_attribute_name = forms.CharField(max_length = 30, label='класс атрибута', required=False, widget = forms.HiddenInput())
-	individ_for_attribute_name = forms.CharField(max_length = 30, label='экземпляр атрибута', required=False, widget = forms.HiddenInput())
+	new_attr_name = forms.CharField(max_length = 30, label='Attribute name')
+	new_attr_value = forms.CharField(max_length = 30, label='Attribute value')
+	subconc_for_attribute_name = forms.CharField(max_length = 30, label='подкласс атрибута', widget = forms.HiddenInput())
+	conc_for_attribute_name = forms.CharField(max_length = 30, label='класс атрибута', widget = forms.HiddenInput())
+	individ_for_attribute_name = forms.CharField(max_length = 30, label='экземпляр атрибута', widget = forms.HiddenInput())
 
 
 class addSubSubConceptIndividualAttrForm(forms.Form):
-	new_attrn = forms.CharField(max_length = 30, label='Attribute name', required=False)
-	new_attrv = forms.CharField(max_length = 30, label='Attribute value', required=False)
-	subcon_for_attribute_name = forms.CharField(max_length = 30, label='подкласс атрибута', required=False, widget = forms.HiddenInput())
-	subsubcon_for_attribute_name = forms.CharField(max_length = 30, label='подподкласс атрибута', required=False, widget = forms.HiddenInput())
-	con_for_attribute_name = forms.CharField(max_length = 30, label='класс атрибута', required=False, widget = forms.HiddenInput())
-	ind_for_attribute_name = forms.CharField(max_length = 30, label='экземпляр атрибута', required=False, widget = forms.HiddenInput())
+	new_attrn = forms.CharField(max_length = 30, label='Attribute name')
+	new_attrv = forms.CharField(max_length = 30, label='Attribute value')
+	subcon_for_attribute_name = forms.CharField(max_length = 30, label='подкласс атрибута', widget = forms.HiddenInput())
+	subsubcon_for_attribute_name = forms.CharField(max_length = 30, label='подподкласс атрибута', widget = forms.HiddenInput())
+	con_for_attribute_name = forms.CharField(max_length = 30, label='класс атрибута', widget = forms.HiddenInput())
+	ind_for_attribute_name = forms.CharField(max_length = 30, label='экземпляр атрибута', widget = forms.HiddenInput())
 
 class DeleteConceptForm(forms.Form):
-	del_concept_name = forms.CharField(max_length = 30, label='класс', required=False, widget = forms.HiddenInput())
+	del_concept_name = forms.CharField(max_length = 30, label='класс', widget = forms.HiddenInput())
 
 class DeleteOntologyForm(forms.Form):
-	del_ontology_name = forms.CharField(max_length = 30, label='класс', required=False, widget = forms.HiddenInput())
+	del_ontology_name = forms.CharField(max_length = 30, label='класс', widget = forms.HiddenInput())
 
 class DeleteConceptIndividualForm(forms.Form):
-	del_concept_ind_name = forms.CharField(max_length = 30, label='класс', required=False, widget = forms.HiddenInput())
-	del_indc_name = forms.CharField(max_length = 30, label='экземпляр', required=False, widget = forms.HiddenInput())
+	del_concept_ind_name = forms.CharField(max_length = 30, label='класс', widget = forms.HiddenInput())
+	del_indc_name = forms.CharField(max_length = 30, label='экземпляр', widget = forms.HiddenInput())
 
 class DeleteSubConceptIndividualForm(forms.Form):
-	del_subconcept_ind_name = forms.CharField(max_length = 30, label='подкласс', required=False, widget = forms.HiddenInput())
-	del_conc_ind_name = forms.CharField(max_length = 30, label='класс', required=False, widget = forms.HiddenInput())
-	del_indsc_name = forms.CharField(max_length = 30, label='экземпляр', required=False, widget = forms.HiddenInput())
+	del_subconcept_ind_name = forms.CharField(max_length = 30, label='подкласс', widget = forms.HiddenInput())
+	del_conc_ind_name = forms.CharField(max_length = 30, label='класс', widget = forms.HiddenInput())
+	del_indsc_name = forms.CharField(max_length = 30, label='экземпляр', widget = forms.HiddenInput())
 
 class DeleteSubSubConceptIndividualForm(forms.Form):
-	del_subsubcon_ind_name = forms.CharField(max_length = 30, label='подподкласс', required=False, widget = forms.HiddenInput())
-	del_subcon_ind_name = forms.CharField(max_length = 30, label='подкласс', required=False, widget = forms.HiddenInput())
-	del_con_ind_name = forms.CharField(max_length = 30, label='класс', required=False, widget = forms.HiddenInput())
-	del_indssc_name = forms.CharField(max_length = 30, label='экземпляр', required=False, widget = forms.HiddenInput())
+	del_subsubcon_ind_name = forms.CharField(max_length = 30, label='подподкласс', widget = forms.HiddenInput())
+	del_subcon_ind_name = forms.CharField(max_length = 30, label='подкласс', widget = forms.HiddenInput())
+	del_con_ind_name = forms.CharField(max_length = 30, label='класс', widget = forms.HiddenInput())
+	del_indssc_name = forms.CharField(max_length = 30, label='экземпляр', widget = forms.HiddenInput())
 
 class DeleteSubConceptForm(forms.Form):
-	concept_name_for_del_sub = forms.CharField(max_length = 30, label='класс', required=False, widget = forms.HiddenInput())
-	del_subconcept_name = forms.CharField(max_length = 30, label='подкласс', required=False, widget = forms.HiddenInput())
+	concept_name_for_del_sub = forms.CharField(max_length = 30, label='класс', widget = forms.HiddenInput())
+	del_subconcept_name = forms.CharField(max_length = 30, label='подкласс', widget = forms.HiddenInput())
 
 class DeleteSubSubConceptForm(forms.Form):
-	concept_name_for_del_sub_sub = forms.CharField(max_length = 30, label='класс', required=False, widget = forms.HiddenInput())
-	sub_concept_name_for_del_sub_sub = forms.CharField(max_length = 30, label='подкласс', required=False, widget = forms.HiddenInput())
-	del_sub_sub_concept_name = forms.CharField(max_length = 30, label='подподкласс', required=False, widget = forms.HiddenInput())
+	concept_name_for_del_sub_sub = forms.CharField(max_length = 30, label='класс', widget = forms.HiddenInput())
+	sub_concept_name_for_del_sub_sub = forms.CharField(max_length = 30, label='подкласс', widget = forms.HiddenInput())
+	del_sub_sub_concept_name = forms.CharField(max_length = 30, label='подподкласс', widget = forms.HiddenInput())
 
 
 class AddIndividualOrSubForm(forms.Form):
-	concept_individual_name = forms.CharField(max_length = 30, label='класс', required=False, widget = forms.HiddenInput())
-	new_individual_name = forms.CharField(max_length = 30, label='Individual', required=False)
-	new_sub_concept_name = forms.CharField(max_length = 30, label='SubConcept', required=False)
+	concept_individual_name = forms.CharField(max_length = 30, label='класс', widget = forms.HiddenInput())
+	new_individual_name = forms.CharField(max_length = 30, label='Individual')
+	new_sub_concept_name = forms.CharField(max_length = 30, label='SubConcept')
 
 class AddIndividualOrSubSubForm(forms.Form):
-	concept_name_for_sub = forms.CharField(max_length = 30, label='класс', required=False, widget = forms.HiddenInput())
-	sub_concept_individual_name = forms.CharField(max_length = 30, label='класс', required=False, widget = forms.HiddenInput())
-	new_sub_individual_name = forms.CharField(max_length = 30, label='Individual', required=False)
-	new_sub_sub_concept_name = forms.CharField(max_length = 30, label='SubConcept', required=False)
+	concept_name_for_sub = forms.CharField(max_length = 30, label='класс', widget = forms.HiddenInput())
+	sub_concept_individual_name = forms.CharField(max_length = 30, label='класс', widget = forms.HiddenInput())
+	new_sub_individual_name = forms.CharField(max_length = 30, label='Individual')
+	new_sub_sub_concept_name = forms.CharField(max_length = 30, label='SubConcept')
 
 class AddIndividualForSubSubForm(forms.Form):
-	concept_name_for_sub_add = forms.CharField(max_length = 30, label='класс', required=False, widget = forms.HiddenInput())
-	sub_concept_individual_name_add = forms.CharField(max_length = 30, label='подкласс', required=False, widget = forms.HiddenInput())
-	sub_sub_concept_individual_name_add = forms.CharField(max_length = 30, label='подподкласс', required=False, widget = forms.HiddenInput())
-	individual_name_for_sub_sub = forms.CharField(max_length = 30, label='Individual', required=False)
+	concept_name_for_sub_add = forms.CharField(max_length = 30, label='класс', widget = forms.HiddenInput())
+	sub_concept_individual_name_add = forms.CharField(max_length = 30, label='подкласс', widget = forms.HiddenInput())
+	sub_sub_concept_individual_name_add = forms.CharField(max_length = 30, label='подподкласс', widget = forms.HiddenInput())
+	individual_name_for_sub_sub = forms.CharField(max_length = 30, label='Individual')
 
 
 class CreateOntologyForm(forms.Form):
-	new_ontology_name = forms.CharField(max_length = 30, label='', required=False)
+	new_ontology_name = forms.CharField(max_length = 30, label='')
 
 class OntologyModelChoiceField(forms.ModelChoiceField):
     def label_from_instance(self, obj):
@@ -98,7 +98,7 @@ class ChoiseOntologyForm(forms.Form):
 		for i in Ontology.objects.all():
 			ontology_choices.append((i.ontology_name, i.ontology_name))
 		print(ontology_choices)
-		self.fields['ontologys'] =forms.ChoiceField(choices=ontology_choices, label='', required=False)
+		self.fields['ontologys'] =forms.ChoiceField(choices=ontology_choices, label='')
 	
 	ontologys = forms.ChoiceField()
 
